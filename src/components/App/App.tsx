2603670769aa3a1fbb7811c749a30d18dbc3f139
Yurii-Masniuk <yeristar@gmail.com>
2025-09-27 22:26:10 +0300

    create movie

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,46 @@
 import { useState } from 'react'
-import axios from 'axios'
 import css from './App.module.css'
 import SearchBar from '../SearchBar/SearchBar'
-import { Toaster } from 'react-hot-toast'
+import { Toaster, toast } from 'react-hot-toast'
+import { fetchMovies } from '../../services/movieService'
+import type { Movie } from '../../types/movie'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-  const handleSearch = (query: string) => {
+  const handleSearch = async (query: string) => {
 
+    setMovies([]);
+    setError(null);
+    setLoading(true);
+
+    try {
+      const fetchedMovies = await fetchMovies(query);
+
+      if (fetchedMovies.length === 0) {
+        toast.error('No movies found for your request.');
+      }
+
+      setMovies(fetchedMovies);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('An unexpected error occurred.');
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <>
+    <div className={css.app}>
       <SearchBar onSubmit={handleSearch} />
       <Toaster />
-
-
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
